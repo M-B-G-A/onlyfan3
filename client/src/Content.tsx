3f5ae4c63464e5f27b95bafb84906bdad9c688d5
Hakyung Kim <hkhk4862@gmail.com>
2023-06-18 21:15:23 +0900

    fix profile

--- client/src/Content.tsx
+++ client/src/Content.tsx
@@ -1,12 +1,13 @@
 import { FeedModel } from './Feed';
+import { ProfileDialog } from './ProfileDialog';
 import AppIcon from './assets/app_icon.svg'
 
-export const Content = ({ feed, handleClickOpen } : { feed: FeedModel, handleClickOpen: () => void }) => {
-    return (
+export const Content = ({ feed, handleClickOpen } : { feed: FeedModel, handleClickOpen: (userId: string) => void }) => {
+  return (
         <div style={{ backgroundColor: "white", width: "800px", borderRadius: "20px", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.11)" }}>
           <div style={{ padding: "30px" }}>
             <div style={{ float: "left", marginRight: "18px" }}>
-              <img src="" style={{ width: "80px", height: "80px", borderRadius: "40px", overflow: "hidden", backgroundColor: "#EEEEEE" }} onClick={handleClickOpen} />
+              <img src="" style={{ width: "80px", height: "80px", borderRadius: "40px", overflow: "hidden", backgroundColor: "#EEEEEE" }} onClick={() => handleClickOpen(feed.id || "")} />
             </div>
             <div style={{ color: "#6C6C6C", fontSize: "14px" }}>@{feed.name}</div>
             <div style={{ color: "#000000", fontSize: "18px" }}>
@@ -16,6 +17,6 @@ export const Content = ({ feed, handleClickOpen } : { feed: FeedModel, handleCli
           <div style={{ width: "100%", margin: "0 auto", textAlign: "center" }}>
             <img src={ feed.image } style={{ maxWidth: "800px", height: "400px", backgroundColor: "#EEEEEE", objectFit: "contain", textAlign: "center" }} />
           </div>
-        </div>
+          </div>
     );
 };
